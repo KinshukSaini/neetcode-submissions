2b0af4f0245cc0ce59767e308bf695e4b5a91124
Kinshuk Saini <137279188+KinshukSaini@users.noreply.github.com>
2026-09-05 14:49:57 +0530

    Add: longest-palindromic-substring - submission-7

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-7.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxLen = 1;
+    int start = 0;
+
+    bool isPal(string& s, int i, int j, vector<vector<int>>& dp) {
+        if (i >= j) return true;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        // 1. Determine if s[i..j] is a palindrome
+        bool curr = (s[i] == s[j]) && isPal(s, i + 1, j - 1, dp);
+
+        if (curr && (j - i + 1 > maxLen)) {
+            maxLen = j - i + 1;
+            start = i;
+        }
+
+        // 2. Explore other substring intervals
+        isPal(s, i + 1, j, dp);
+        isPal(s, i, j - 1, dp);
+
+        // 3. Cache whether s[i..j] is a palindrome
+        return dp[i][j] = curr ? 1 : 0;
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n <= 1) return s;
+
+        maxLen = 1;
+        start = 0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        isPal(s, 0, n - 1, dp);
+
+        return s.substr(start, maxLen);
+    }
+};
